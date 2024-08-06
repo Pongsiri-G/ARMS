4d719e71864d079a43cc92ec9ecab54b60f635c5
ThitichotK <iamfirst251726@gmail.com>
2024-08-06 12:33:15 +0700

    Add login page with controller / stylesheet and icon images

---
+++ src/main/java/ku/cs/controllers/LoginController.java
@@ -0,0 +1,20 @@
+package ku.cs.controllers;
+import javafx.scene.control.Label;
+import ku.cs.services.FXRouter;
+
+import javafx.fxml.FXML;
+import java.io.IOException;
+
+public class LoginController {
+
+    /*
+    @FXML
+    protected void onRegisterButtonClick() {
+        try {
+            FXRouter.goTo("register");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    */
+}
--- src/main/java/ku/cs/cs211671project/MainApplication.java
+++ src/main/java/ku/cs/cs211671project/MainApplication.java
@@ -11,14 +11,14 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXRouter.bind(this, stage, "CS211 Project", 1024, 768);
+        FXRouter.bind(this, stage, "CS211 Project", 1440, 1024);
         configRoutes();
-        FXRouter.goTo("hello");
+        FXRouter.goTo("login");
     }
 
     private void configRoutes() {
         String viewPath = "ku/cs/views/";
-        FXRouter.when("hello", viewPath + "hello-view.fxml");
+        FXRouter.when("login", viewPath + "login-view.fxml");
     }
 
     public static void main(String[] args) {
