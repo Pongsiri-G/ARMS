3b5aad59d0833fb1acf7bd79efd69c513be1b935
Pongsiri <Pongsiri__G@gmail.com>
2024-08-25 15:21:59 +0700

    Refactor view to give

--- src/main/java/ku/cs/models/Department.java
+++ src/main/java/ku/cs/models/Department.java
@@ -1,2 +1,56 @@
-package ku.cs.models;public class Department {
+package ku.cs.models;
+
+import java.util.Random;
+
+public class Department {
+    private String departmentName;
+    private String departmentID;
+
+    // Begin Constructor
+    Department(String departmentName){
+        // Auto Generate departmentID will implement later
+        Random rand = new Random();
+        this.departmentName = departmentName;
+        this.departmentID = rand.nextInt(90) + 10 + "";
+    }
+
+    Department(String departmentName, String departmentID) {
+        this.departmentName = departmentName;
+        this.departmentID = departmentID;
+    }
+    // End Constructor
+
+    public boolean isDepartmentName(String departmentName){
+        return this.departmentName.equals(departmentName);
+    }
+
+    public boolean isDepartmentID(String departmentID){
+        return this.departmentID.equals(departmentID);
+    }
+
+    // Begin Setter
+    public void setDepartmentName(String departmentName){
+        this.departmentName = departmentName;
+    }
+    public void setDepartmentID(String departmentID){
+        this.departmentID = departmentID;
+    }
+    // End Setter
+
+    // Begin Getter
+    public String getDepartmentName(){
+        return this.departmentName;
+    }
+    public String getDepartmentID(){
+        return this.departmentID;
+    }
+    // End Getter
+
+    @Override
+    public String toString(){
+        return departmentName + "\t" + departmentID;
+    }
+
+
+
 }
