888d16298c0a8f245eaf550e2a3e23f4a4807c43
Pongsiri <Pongsiri__G@gmail.com>
2024-09-02 12:45:42 +0700

    create-Request-Manager-Method

--- src/main/java/ku/cs/models/FacultyStaff.java
+++ src/main/java/ku/cs/models/FacultyStaff.java
@@ -1,11 +1,45 @@
 package ku.cs.models;
 
+import java.util.ArrayList;
+
 public class FacultyStaff extends User {
-    Faculty faculty;
+    private Faculty faculty;
+    private ArrayList<RequestManager> requestManagers;
+
+    // Begin Constructor
     public FacultyStaff(String username, String password, String name, Faculty faculty) {
         super(username, password, name);
         this.faculty = faculty;
     }
+    // End Constructor
+
+    // Begin Handle Request Managers
+    public void loadRequestManagers() {
+        // Will load DataSource later
+        requestManagers = new ArrayList<>();
+    }
+    public void writeRequestManagers() {
+        return;
+    }
+
+    public void addRequestManager(String name, String position){
+        requestManagers.add(new RequestManager(name, position + faculty.getFacultyName()));
+    }
+    public void removeRequestManager(RequestManager requestManager){
+        requestManagers.remove(requestManager);
+    }
+    public void changeRequestManagerName(RequestManager requestManager, String name){
+        requestManager.setName(name);
+    }
+    public void changeRequestManagerPosition(RequestManager requestManager, String position){
+        requestManager.setPosition(position + faculty.getFacultyName());
+    }
+    public ArrayList<RequestManager> getRequestManagers() {
+        return requestManagers;
+    }
+    // End Handle Request Manager
+
+
     @Override
     public Faculty getFaculty() {
         return faculty;
