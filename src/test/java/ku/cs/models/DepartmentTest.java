593e2e3eff8661349a1cbb921fd8468826479e4e
Pongsiri-G <Pongsiri__G@hotmail.com>
2024-09-12 15:47:14 +0700

    test

--- src/test/java/ku/cs/models/DepartmentTest.java
+++ src/test/java/ku/cs/models/DepartmentTest.java
@@ -34,6 +34,7 @@ class DepartmentTest {
         Department engineering  = new Department("Engineering", faculty);
         System.out.println(engineering);
         assertNotNull(engineering);
+        //g
     }
 
 
