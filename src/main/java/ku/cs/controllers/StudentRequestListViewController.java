50a220190cb61ea195e4f6d6ce5f40997a672a29
FirstGameGG <iamfirst251726@gmail.com>
2024-10-08 18:59:18 +0700

    Add Tabelview for StudentRequestViewController

--- src/main/java/ku/cs/controllers/StudentRequestListViewController.java
+++ src/main/java/ku/cs/controllers/StudentRequestListViewController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.geometry.Bounds;
 import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -14,12 +15,16 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import ku.cs.models.Request;
+import ku.cs.models.RequestList;
 import ku.cs.models.Student;
 import ku.cs.models.UserList;
 import ku.cs.services.FXRouter;
+import ku.cs.services.RequestListFileDatasource;
 import ku.cs.services.UserListFileDatasource;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class StudentRequestListViewController {
 
@@ -33,19 +38,31 @@ public class StudentRequestListViewController {
 
     @FXML private Label usernameLabel;
 
+    @FXML private Label doneLabel;
+
+    @FXML private Label waitLabel;
+
+    @FXML private Label rejectLabel;
+
     @FXML private Rectangle currentBar1;
 
     @FXML private Rectangle currentBar2;
 
+    @FXML private TableView<Request> requestListTableview;
+
     @FXML private Circle profilePictureDisplay;
 
     private UserList userList;
-    private UserListFileDatasource datasource;
+    private RequestList requestList;
+    private UserListFileDatasource userListDatasource;
+    private RequestListFileDatasource requestListDatasource;
     private Student student;
 
     public StudentRequestListViewController(){
-        datasource = new UserListFileDatasource("data/test", "studentlist.csv", "advisorlist.csv", "facultyofficerlist.csv","departmentofficerlist.csv", "facdeplist.csv");
-        this.userList = datasource.readData();
+        userListDatasource = new UserListFileDatasource("data/test", "studentlist.csv", "advisorlist.csv", "facultyofficerlist.csv","departmentofficerlist.csv", "facdeplist.csv");
+        requestListDatasource = new RequestListFileDatasource("data/test", "requestlist.csv");
+        this.userList = userListDatasource.readData();
+        this.requestList = requestListDatasource.readData();
     }
 
     @FXML
@@ -56,6 +73,10 @@ public class StudentRequestListViewController {
         usernameLabel.setText(student.getUsername());
         currentBar1.setVisible(false);
         setProfilePicture(student.getProfilePicturePath());
+        doneLabel.setText(String.format("%d", student.getStudentApprovedRequestCount(student.getRequestsByStudent(requestList))));
+        waitLabel.setText(String.format("%d", student.getStudentPendingRequestCount(student.getRequestsByStudent(requestList))));
+        rejectLabel.setText(String.format("%d", student.getStudentRejectedRequestCount(student.getRequestsByStudent(requestList))));
+        showTable(student.getRequestsByStudent(requestList));
         System.out.println("[" + student.getName() + " " + student.getUsername() + "]");
     }
 
@@ -71,6 +92,61 @@ public class StudentRequestListViewController {
             profilePictureDisplay.setFill(Color.GRAY);
         }
     }
+
+    private void showTable(ArrayList<Request> requestList) {
+        TableColumn<Request, String> requestTypeColumn = new TableColumn<>("ประเภทคำร้อง");
+        requestTypeColumn.setCellValueFactory(new PropertyValueFactory<>("requestType"));
+
+        TableColumn<Request, String> requestStatusColumn = new TableColumn<>("สถานะคำร้อง");
+        requestStatusColumn.setCellValueFactory(new PropertyValueFactory<>("recentStatusLog"));
+
+        requestStatusColumn.setCellFactory(column -> new TableCell<Request, String>() {
+            @Override
+            protected void updateItem(String statusLog, boolean empty) {
+                super.updateItem(statusLog, empty);
+                if (empty || statusLog == null) {
+                    setText(null);
+                    setStyle("");
+                } else {
+                    setText(statusLog);
+
+                    Request request = getTableView().getItems().get(getIndex());
+                    String status = request.getStatus();
+
+                    switch (status) {
+                        case "กำลังดำเนินการ":
+                            setStyle("-fx-text-fill: #d7a700;");
+                            break;
+                        case "ปฏิเสธ":
+                            setStyle("-fx-text-fill: #be0000;");
+                            break;
+                        case "เสร็จสิ้น":
+                            setStyle("-fx-text-fill: #149100;");
+                            break;
+                        default:
+                            setStyle("");
+                            break;
+                    }
+                }
+            }
+        });
+
+        TableColumn<Request, String> lastModifiedColumn = new TableColumn<>("วันที่แก้ไขล่าสุด");
+        lastModifiedColumn.setCellValueFactory(new PropertyValueFactory<>("lastModifiedDateTime"));
+
+        // Clear previous columns and add the new ones
+        requestListTableview.getColumns().clear();
+        requestListTableview.getColumns().add(requestTypeColumn);
+        requestListTableview.getColumns().add(requestStatusColumn);
+        requestListTableview.getColumns().add(lastModifiedColumn);
+
+        // Add the request list to the TableView
+        requestListTableview.getItems().clear();
+        requestListTableview.getItems().addAll(requestList);
+    }
+
+
+
 
     @FXML
     public void logoutClick(MouseEvent event) throws IOException {
